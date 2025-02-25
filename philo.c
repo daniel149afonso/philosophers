960d9842ab7a2cea42b89746b8ceef8848d630df
daniel <daniel-congo2@hotmail.com>
2025-02-25 16:36:17 +0100

    creation struct mutex, fork, philo

---
+++ philo.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daniel149afonso <daniel149afonso@studen    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/18 15:09:20 by daniel149af       #+#    #+#             */
+/*   Updated: 2025/02/18 15:15:49 by daniel149af      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "philo.h"
+
+void    philo(int philo_N, int dieT, int eatT, int sleepT)
+{
+    int fork;
+    if (philo_N == 1)
+        fork
+    
+}
+
+int main(int argc, char **argv)
+{
+    if (argc != 5)
+        return ;
+    philo(argv[2],argv[3],argv[4],argv[5]);
+    return (0);
+}
---
+++ philo.h
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daniel149afonso <daniel149afonso@studen    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/18 15:09:56 by daniel149af       #+#    #+#             */
+/*   Updated: 2025/02/25 15:00:02 by daniel149af      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h> // printf
+# include <unistd.h> //write, usleep
+# include <stdlib.h> // malloc free
+# include <pthread.h> // mutex: init destroy, lock unlock
+                    // threads: create, join and detach
+# include <sys/time.h> // gettimeofday
+# include <limits.h> // INT_MAX
+
+//BOOL
+typedef enum s_bool
+{
+    false,
+    true
+}  t_bool;
+
+//MUTEX
+typedef pthread_mutex_t t_mtx;
+
+//FORK
+typedef struct s_fork
+{
+    t_mtx   fork;
+    int     fork_id;
+}   t_fork;
+
+//PHILO
+typedef struct s_philo
+{
+    int     id;
+    long    meals_counter;
+    t_bool  full;
+    long    last_meal_time; // time passed after last meal
+    t_fork  left_fork;
+    t_fork  right_fork;
+    pthread_t   thread_id; // a philo is a thread
+}   t_philo;
+#endif
